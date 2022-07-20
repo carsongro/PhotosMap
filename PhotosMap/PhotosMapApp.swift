f3004805ac6193f168b09434d2a260fc77903f88
Carson <carsongro@gmail.com>
2022-07-20 09:57:03 -0600

    Initial Commit

---
+++ PhotosMap/PhotosMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosMapApp.swift
+//  PhotosMap
+//
+//  Created by Carson Gross on 7/20/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotosMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
